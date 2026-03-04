84084b2eb42efe5412a1e384caae312ff58cc298
Juan Gonzalo Alvarez <juangonzalo22@gmail.com>
2026-03-04 18:48:07 -0500

    Creación del Footer con sus estilos

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+    <footer className="footer">
+        <span>© {new Date().getFullYear()} Spendy. Todos los derechos reservados.</span>
+    </footer>
+    )
+}
+
+export default Footer
