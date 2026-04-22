e811e21abce6a54299d21ed5ddf28652623dfe37
Sofia Escobar <sofiasofiaescobar1@gmail.com>
2026-04-21 21:01:34 -0500

    chore: Definición de endpoints y urls de la api

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+const URL_BASE = "localhost:8080/"
+
+export let end_ponits = {
+    users: URL_BASE + "users",
+    merchants: URL_BASE + "merchants",
+    expenses: URL_BASE + "expenses/",
+    payment_methos: URL_BASE +"payment_methos",
+    category: URL_BASE + "category",
+
+}
